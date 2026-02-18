129879316dafd283244e2cada5e9f0c73145d85e
Lab User <labuser@lb443mac22.dcism.org>
2026-02-18 12:12:03 +0800

    Initial commit

---
+++ BMI CALC/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BMI_CALC;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BMI CALC/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace BMI_CALC;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void BMICalc(object sender, EventArgs e)
+    {
+        double heightInCM = Convert.ToDouble(gettingHeight.Text);
+        double WeightInKg = Convert.ToDouble(gettingWeight.Text);
+
+        if (!double.IsNaN(heightInCM) && !double.IsNaN(WeightInKg))
+        {
+            double BMI = (WeightInKg / (heightInCM * heightInCM)) * 10000;
+
+            Result.Text = "Your BMI is " + Math.Round(BMI, 2);
+            
+            if (BMI < 18.5)
+            {
+                Status.Text = "You are Underweight";
+            } 
+            else if (BMI < 25)
+            {
+                Status.Text = "You have a Normal BMI";
+            } 
+            else if (BMI < 30)
+                Status.Text = "You are Overweight";
+            else
+            {
+                Status.Text = "You are Obese";
+            }
+        }
+        else
+        {
+            Status.Text = "Please Input Proper Values";
+        }      
+    }
+}
