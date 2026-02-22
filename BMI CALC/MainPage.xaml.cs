b55dc7dfb6e0bab849a347b1378128c02cc1b561
HerbalFern <rosellofernandrei@gmail.com>
2026-02-22 21:05:49 +0800

    minor changes to color n stuff

--- BMI CALC/MainPage.xaml.cs
+++ BMI CALC/MainPage.xaml.cs
@@ -6,36 +6,41 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
-
+    
+    private async void HideKeyboard()
+    {
+        await MainThread.InvokeOnMainThreadAsync(() =>
+        {
+            gettingHeight.Unfocus();
+            gettingWeight.Unfocus();
+        });
+    }
+    
     private async void BMICalc(object sender, EventArgs e)
     {
-        double heightInCM = Convert.ToDouble(gettingHeight.Text);
-        double WeightInKg = Convert.ToDouble(gettingWeight.Text);
-
-        if (!double.IsNaN(heightInCM) && !double.IsNaN(WeightInKg))
+        // nag double try parse daw is better for input handling? 
+        
+        if (double.TryParse(gettingHeight.Text, out double heightInCM) &&
+            double.TryParse(gettingWeight.Text, out double WeightInKg))
         {
             double BMI = (WeightInKg / (heightInCM * heightInCM)) * 10000;
-
             Result.Text = "Your BMI is " + Math.Round(BMI, 2);
-            
+
             if (BMI < 18.5)
-            {
                 Status.Text = "You are Underweight";
-            } 
             else if (BMI < 25)
-            {
                 Status.Text = "You have a Normal BMI";
-            } 
             else if (BMI < 30)
                 Status.Text = "You are Overweight";
             else
-            {
                 Status.Text = "You are Obese";
-            }
         }
         else
         {
             Status.Text = "Please Input Proper Values";
-        }      
+        }
+        
+        HideKeyboard(); // yeah doesnt work lol
+        
     }
 }
